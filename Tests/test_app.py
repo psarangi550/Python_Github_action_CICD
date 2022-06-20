5df0f2b05ee7d5e35f4f993310aa6d19e2f2d8b8
root <root@TE1GXB2J3.iuser.iroot.adidom.com>
2022-06-20 08:34:05 +0530

    all push again

--- Tests/test_app.py
+++ Tests/test_app.py
@@ -1,7 +1,9 @@
 from src.app import home, index
 
+
 def test_flask_app():
-    assert home()=="Hello, world!"
+    assert home() == "Hello, world!"
+
 
 def test_flask_new_app():
-    assert index()=="<h1>good Afternoon</h1>"
+    assert index() == "<h1>good Afternoon</h1>"
--- src/app.py
+++ src/app.py
@@ -1,14 +1,17 @@
-from  flask import Flask
+from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return "Hello, world!"
 
+
 @app.route('/index')
 def index():
     return "<h1>good Afternoon</h1>"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
